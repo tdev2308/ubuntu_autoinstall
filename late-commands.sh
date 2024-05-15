ea51ba7666beb84c8571813d36213521b3ebc4a2
tdev2308 <20651029+tdev2308@users.noreply.github.com>
2024-05-15 09:58:18 -0700

    Add files via upload

---
+++ late-commands.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+
+{
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list
+apt-get update
+mkdir /etc/docker
+cat >/etc/docker/daemon.json <<EOF
+{
+    "storage-driver": "zfs"
+}
+EOF
+#echo "{  \"storage-driver\": \"zfs\"  }" > /etc/docker/daemon.json
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# enroll MOK for NVIDIA driver
+# mokutil -gpassword "test" > /tmp/mokpwd
+# mokutil --import /var/lib/shim-signed/mok/MOK.der -f /tmp/mokpwd
+# install nvidia-container-toolkit
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | \
+  gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+  sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+  tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+apt-get update
+apt-get install -y nvidia-container-toolkit
+} &> /var/log/late-commands.log
